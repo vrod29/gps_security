16e76da605e7f0659e6aee77ddf71b4a5b7623fc
Unknown <vmrdz220@gmail.com>
2018-05-10 12:12:03 -0500

    site complete

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-$('.hamburger').click(function(e){
-  e.preventDefault();
-  $(this).toggleClass('is-active');
-})
-
-$(window).scroll(function(){
-  $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-});
---
+++ js/map-script.js
@@ -0,0 +1,200 @@
+"use strict";
+
+
+function goMap () {
+  if ($('#map').length) {
+        // Styles a map in night mode.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 26.1799335, lng: -98.2938492},
+          zoom: 12,
+          scrollwheel: false,
+          styles: [ {
+		        "featureType": "all",
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		            {
+		                "saturation": 36
+		            },
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 40
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "all",
+		        "elementType": "labels.text.stroke",
+		        "stylers": [
+		            {
+		                "visibility": "on"
+		            },
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 16
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "all",
+		        "elementType": "labels.icon",
+		        "stylers": [
+		            {
+		                "visibility": "off"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "administrative",
+		        "elementType": "geometry.fill",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 20
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "administrative",
+		        "elementType": "geometry.stroke",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 17
+		            },
+		            {
+		                "weight": 1.2
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "landscape",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 20
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "poi",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 21
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.highway",
+		        "elementType": "geometry.fill",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 17
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.highway",
+		        "elementType": "geometry.stroke",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 29
+		            },
+		            {
+		                "weight": 0.2
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.arterial",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 18
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.local",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 16
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "transit",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 19
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "water",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#000000"
+		            },
+		            {
+		                "lightness": 17
+		            }
+		        ]
+		    }
+          ]
+        });
+
+		// To add the marker to the map
+		  var image = 'images/logo/map.png';
+		  var beachMarker = new google.maps.Marker({
+		    position: {lat: 26.1799335, lng: -98.2938492},
+		    map: map,
+		    icon: image,
+		    title:"GPS Office!",
+		    infoWindow: {
+	            content: '<h6>GPS office</h6>'
+	          }
+		  });
+  };
+};
+
+
+// Dom Ready Function
+jQuery(document).on('ready', function () {
+	(function ($) {
+		// add your functions
+		goMap()
+	})(jQuery);
+});
